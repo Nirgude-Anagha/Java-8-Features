591435e4a3c5cda02fa597a6de92ee2047debed3
Anagha Nirgude <anagha.nirgude1@gmail.com>
2025-11-11 00:13:03 +0530

    Predicate  example

--- src/main/java/org/example/functionalinterfaces/PredicateExample.java
+++ src/main/java/org/example/functionalinterfaces/PredicateExample.java
@@ -1,10 +1,18 @@
 package org.example.functionalinterfaces;
 
+import org.example.functionalinterfaces.data.Student;
+import org.example.functionalinterfaces.data.StudentDatabase;
+
+import java.util.ArrayList;
+import java.util.List;
 import java.util.function.Predicate;
 
 public class PredicateExample {
     static Predicate<Integer> p = i -> i %2 == 0;
     static Predicate<Integer> p1 = i -> i %5 == 0;
+
+    static Predicate<Student> studentPredicate1 = s -> s.getGradeLevel()>=3;
+    static Predicate<Student> studentPredicate2 = s -> s.getGpa()>=3.2;
 
     static void predicateAnd(){
         System.out.println(p.and(p1).test(10));
@@ -19,6 +27,15 @@ public class PredicateExample {
         System.out.println(p.or(p1).negate().test(9));
     }
 
+    static void predicateStudent(){
+        List<Student> students = StudentDatabase.getStudents();
+        students.forEach(student -> {
+            if(studentPredicate1.and(studentPredicate2).test(student)){
+                System.out.println(student);
+            }
+        });
+    }
+
     static void main() {
 
         System.out.println(p.test(2));
@@ -28,5 +45,7 @@ public class PredicateExample {
         predicateOr();
         System.out.println("---------------predicateNegate-----------------");
         predicateNegate();
+        System.out.println("---------------predicateStudent-----------------");
+        predicateStudent();
     }
 }
